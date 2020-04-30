6f4f74fe2d164af5e9e6e05d633157d69861b6b5
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-04-30 22:40:19 +0300

    Update refactoring-sample

--- BeforeRefactoring/XmlProcessor.php
+++ BeforeRefactoring/XmlProcessor.php
@@ -1,2 +1,20 @@
 <?php
+function process ($node) {
+  $child = $node -> childNodes;
+  foreach ($child as $element) {
+    if ($element -> nodeType == XML_TEXT_NODE) {
+      if (strlen(trim($element->nodeValue))) {
+        echo(trim($element->nodeValue));
+        echo("<br>");
+      }
+    } else if ($element -> nodeType == XML_ELEMENT_NODE) {
+      process($element);
+    }
+  }
+}
+
+$dom = new DOMDocument();
+$dom->load("../Data/contest.xml");
+$root = $dom->documentElement;
+process($root);
 ?>
