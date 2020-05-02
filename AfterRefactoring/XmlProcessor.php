55b9dd5c5b797b2cd2a4fdde0f00cd8c8bbbc08e
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-02 22:53:52 +0300

    Update better XMLProcessor.php

--- AfterRefactoring/XmlProcessor.php
+++ AfterRefactoring/XmlProcessor.php
@@ -1,2 +1,41 @@
 <?php
+
+class XMLDataProcessor {
+	
+	private function OutputTrimData ($ValueToTrim) {
+		echo(trim($ValueToTrim));
+		echo("</br>");
+	}
+
+	private function ProcessXMLData ($Node) {
+
+	  $Child = $Node -> childNodes;
+
+	  foreach ($Child as $Element)
+
+	    if ($Element -> nodeType == XML_TEXT_NODE) {
+
+	      if (strlen(trim($Element->nodeValue)))
+	      	$this->OutputTrimData($Element->nodeValue);
+
+	    } else if ($Element -> nodeType == XML_ELEMENT_NODE)
+	      $this->ProcessXMLData($Element);
+	}
+
+	public function SetupProcess ($DOM) {
+		
+		$DOM->load("../Data/contest.xml");
+		
+		$root = $DOM->documentElement;
+		
+		$this->ProcessXMLData($root);
+	}
+}
+
+$Processor = new XMLDataProcessor();
+
+$DOM = new DOMDocument();
+
+$Processor->SetupProcess($DOM);
+
 ?>
